15934dba5d726a485584f3080e61d91f411b11ae
Paul <paul@technove.co>
2025-12-01 09:29:06 -0600

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,3 @@
-
 #[aoc::main(01)]
 fn main(input: &str) -> (usize, usize) {
     let xs = input.split('\n').map(|l| {
@@ -12,21 +11,21 @@ fn main(input: &str) -> (usize, usize) {
         let value = value_as_str.parse::<i32>().unwrap();
 
         match direction {
-            &"L" => current_value = current_value + value,
-            &"R" => current_value = current_value - value,
+            &"L" => current_value = current_value - value,
+            &"R" => current_value = current_value + value,
             _ => panic!("no good?")
         }
 
         while current_value < 0 {
-            println!("less than 0, wrapping {current_value}");
+            // println!("less than 0, wrapping {current_value}");
             current_value = 100 + current_value;
-            println!("now have {current_value}");
+            // println!("now have {current_value}");
         }
 
         while current_value > 99 {
-            println!("more than 99, wrapping {current_value}");
+            // println!("more than 99, wrapping {current_value}");
             current_value = current_value - 100;
-            println!("now have {current_value}");
+            // println!("now have {current_value}");
         }
 
         if current_value == 0 {
@@ -34,5 +33,25 @@ fn main(input: &str) -> (usize, usize) {
         }
     }
 
-    (times_hit_zero, 0)
+    current_value = 50; // reset
+    let mut times_passed_zero = 0;
+
+    for (direction, value_as_str) in xs.iter() {
+        let value = value_as_str.parse::<i64>().unwrap();
+        let modifier = if direction == &"L" { -1 } else { 1 };
+
+        // stuck, so let's just try to emulate the exact logic regardless if it's more logic
+        // go over each tick
+        for _ in 0..value {
+            current_value += modifier;
+
+            if current_value % 100 == 0 {
+                println!("zero! {current_value}");
+                times_passed_zero += 1;
+            }
+        }
+        // this actually ended up being shorter
+    }
+
+    (times_hit_zero, times_passed_zero)
 }
